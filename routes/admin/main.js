7a1e9ffd39800335ee3f03f5987b45dd39236e66
mariccardo <riccardo.masetti.work@gmail.com>
2016-11-03 00:35:40 +0100

    loginsession

--- routes/admin/main.js
+++ routes/admin/main.js
@@ -2,14 +2,17 @@ var express = require('express');
 var router = express.Router();
 var session = require('express-session');
 
-if(!session.isLoggedIn)
-    session.isLoggedIn = false;
 
-// define the home page route
 router.post('/api/login', function(req, res) {
+    req.session.isLoggedIn = true;
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ a: 1 }, null, 3));
-    session.isLoggedIn = true;
+});
+
+router.post('/api/logout', function(req, res) {
+    req.session.isLoggedIn = false;
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ a: 0 }, null, 3));
 });
 
 router.use(function secure(req, res, next) {
